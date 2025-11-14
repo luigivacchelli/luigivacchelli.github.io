dcfc04994f5b7c769d8cb363d2b80efaa7b2960a
luigivacchelli <ggvacchelli@gmail.com>
2025-11-14 18:14:11 +0100

    newbio

--- script.js
+++ script.js
@@ -17,17 +17,19 @@
      // --- DICTIONARY FOR TRANSLATIONS ---
      const translations = {
          'en': {
-             welcomeMessage: "This site uses your webcam to enhance the experience. <br> Please don't be shy!",
+             welcomeMessage: `This site uses your webcam to enhance the experience. <br> Please don't be shy!`,
              aboutLink: "whoami",
              easterEggLeftHeader: "what are you doing up so late?",
              easterEggRightHeader: "go to sleep!",
+             aboutBio: `Director and photographer, born in '98. I live in Rome, but my heart is in Genoa. <br> I work on short films, photographic projects and international film productions. <br> There's a good chance I'll beat you at <a href="https://www.chess.com/member/lksmrkds" target="_blank" rel="noopener noreferrer">chess</a>.`,
              copyright: "© 2025 Luigi Vacchelli. All rights reserved."
          },
          'it': {
-             welcomeMessage: "Questo sito richiede l'accesso alla webcam per essere più bello.<br> Non fate i timidi!",
+             welcomeMessage: `Questo sito richiede l'accesso alla webcam per essere più bello.<br> Non fate i timidi!`,
              aboutLink: "chisono",
              easterEggLeftHeader: "cosa ci fai ancora in piedi?",
              easterEggRightHeader: "corri a letto!",
+             aboutBio: `Regista e fotografo, classe '98. Vivo a Roma, ma ho lasciato il cuore a Genova. <br> Lavoro a cortometraggi, progetti fotografici e produzioni cinematografiche internazionali. <br> Ci sono buone probabilità che ti batta a <a href="https://www.chess.com/member/lksmrkds" target="_blank" rel="noopener noreferrer">scacchi</a>.`,
              copyright: "© 2025 Luigi Vacchelli. Tutti i diritti riservati."
          }
      };
